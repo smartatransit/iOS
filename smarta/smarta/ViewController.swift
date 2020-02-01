c608bf2adb7b2159df47c9b360a7812428551b22
Julian A. Fordyce <julian.a.fordyce@gmail.com>
2020-01-31 20:54:42 -0500

    Add starter Xcode project.

---
+++ smarta/smarta/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  smarta
+//
+//  Created by Julian A. Fordyce on 1/31/20.
+//  Copyright © 2020 Julian A. Fordyce. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
