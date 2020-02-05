84a0c7141865f6e48b711fc35318bc36d786dd15
Julian A. Fordyce <julian.a.fordyce@gmail.com>
2020-02-04 20:44:51 -0500

    Create basic model.

--- smarta/smarta/Station.swift
+++ smarta/smarta/Station.swift
@@ -7,3 +7,49 @@
 //
 
 import Foundation
+
+// MARK: - Station
+struct Station: Codable {
+    let station: StationClass
+    let schedule: Schedule
+}
+
+// MARK: - Schedule
+struct Schedule: Codable {
+    let trainID, nextStation: String
+    let destination: Destination
+    let nextArrival, waitingSeconds, waitingTime, eventTime: String
+
+    enum CodingKeys: String, CodingKey {
+        case trainID = "train-id"
+        case nextStation = "next-station"
+        case destination
+        case nextArrival = "next-arrival"
+        case waitingSeconds = "waiting-seconds"
+        case waitingTime = "waiting-time"
+        case eventTime = "event-time"
+    }
+}
+
+enum Destination: String, Codable {
+    case airportStation = "Airport Station"
+    case northSpringsStation = "North Springs Station"
+}
+
+// MARK: - StationClass
+struct StationClass: Codable {
+    let name: String
+    let line: Line
+    let direction: Direction
+}
+
+enum Direction: String, Codable {
+    case north = "N"
+    case south = "S"
+    case east = "E"
+    case west = "W"
+}
+
+enum Line: String, Codable {
+    case red = "Red"
+}
