a6e1cce71c83edbdaa819279702de6d67331ee4d
Julian A. Fordyce <julian.a.fordyce@gmail.com>
2020-02-04 20:42:34 -0500

    Create initial model to represent JSON response

---
+++ smarta/smarta/Station.swift
@@ -0,0 +1,9 @@
+//
+//  Station.swift
+//  smarta
+//
+//  Created by Julian A. Fordyce on 2/4/20.
+//  Copyright © 2020 Julian A. Fordyce. All rights reserved.
+//
+
+import Foundation
--- smarta/smarta/StationsTableViewController.swift
+++ smarta/smarta/StationsTableViewController.swift
@@ -12,6 +12,8 @@ class StationsTableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
 
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
