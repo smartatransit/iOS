b43b10fbc88418f29c1fd9435fd6b8d448e0c89c
Julian A. Fordyce <julian.a.fordyce@gmail.com>
2020-02-28 18:36:50 -0500

    Getting there.

--- smarta/smarta/Table View Controllers/StationTableViewController.swift
+++ smarta/smarta/Table View Controllers/StationTableViewController.swift
@@ -8,11 +8,16 @@
 
 import UIKit
 
-class StationTableViewController: UITableViewController, HasStationSource {
+class StationTableViewController: UIViewController, HasStationSource, UITableViewDataSource, UITableViewDelegate {
+    
+    let tableView = UITableView()
+    
     
     let networkController = NetworkController()
     var source: String
     var stations: [Station] = []
+    
+    
     
     init(source: String) {
         self.source = source
@@ -26,6 +31,13 @@ class StationTableViewController: UITableViewController, HasStationSource {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        tableView.delegate = self as! UITableViewDelegate
+        tableView.dataSource = self as! UITableViewDataSource
+        
+        setupLayout()
+        view.backgroundColor = .white
         networkController.fetchStationInfo(by: source) { (result) in
             do {
                 let stations = try result.get()
@@ -48,24 +60,24 @@ class StationTableViewController: UITableViewController, HasStationSource {
 
     // MARK: - Table view data source
 //
-//    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        return stations.count
-//    }
+     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return stations.count
+    }
 //
-//    
-//    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//        let cell = tableView.dequeueReusableCell(withIdentifier: "CELL", for: indexPath)
 //
-////        guard let cell = tableView.dequeueReusableCell(withIdentifier: StationDetailTableViewCell.reuseIdentifier, for: indexPath) as? StationDetailTableViewCell else { return UITableViewCell() }
-//        let station = stations[indexPath.row]
-////        cell.waitingTimeLabel.text = station.schedule.waitingTime
-////        cell.arrivalTimeLabel.text = station.schedule.nextArrival
-////        cell.selectionStyle = .none
-////        
-//        cell.textLabel?.text = station.schedule.waitingTime
-//        cell.detailTextLabel?.text = station.schedule.waitingTime
-//        return cell
-//    }
+     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+
+//        guard let cell = tableView.dequeueReusableCell(withIdentifier: StationDetailTableViewCell.reuseIdentifier, for: indexPath) as? StationDetailTableViewCell else { return UITableViewCell() }
+        let station = stations[indexPath.row]
+//        cell.waitingTimeLabel.text = station.schedule.waitingTime
+//        cell.arrivalTimeLabel.text = station.schedule.nextArrival
+//        cell.selectionStyle = .none
+//
+        cell.textLabel?.text = station.schedule.waitingTime
+        cell.detailTextLabel?.text = station.schedule.waitingTime
+        return cell
+    }
     
 
     /*
@@ -114,7 +126,16 @@ class StationTableViewController: UITableViewController, HasStationSource {
     */
     
     func setupLayout() {
+        view.addSubview(tableView)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.topAnchor.constraint(equalTo: view.topAnchor, constant: 0).isActive = true
         
+        tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0).isActive = true
+        
+        tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0).isActive = true
+        
+        tableView.widthAnchor.constraint(equalTo: view.widthAnchor, constant: 0).isActive = true
     }
 
 }
+ 
